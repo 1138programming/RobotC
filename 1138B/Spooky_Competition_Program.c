276ba0c79ca30d68940c2d3a9a8ad740242c7336
1138programming <1138programming@gmail.com>
2016-11-17 15:03:25 -0800

    Spooky Competition Program with LCD

--- 1138B/Spooky_Competition_Program.c
+++ 1138B/Spooky_Competition_Program.c
@@ -19,9 +19,145 @@
 //Main competition background code...do not modify!
 #include "Vex_Competition_Includes.c"
 
+const short leftButton = 1;
+const short centerButton = 2;
+const short rightButton = 4;
+int autonNumber = 0;
+
+//Wait for Press--------------------------------------------------
+void waitForPress()
+{
+	while(nLCDButtons == 0){}
+	wait1Msec(5);
+}
+//----------------------------------------------------------------
+
+//Wait for Release------------------------------------------------
+void waitForRelease()
+{
+	while(nLCDButtons != 0){}
+	wait1Msec(5);
+}
+
 void pre_auton()
 {
-  bStopTasksBetweenModes = true;
+	bStopTasksBetweenModes = true;
+	/*------------------------LCD Program Start Here--------------------------*/
+	bLCDBacklight = true;                                    // Turn on LCD Backlight
+	string mainBattery, backupBattery;
+	clearLCDLine(0);                                            // Clear line 1 (0) of the LCD
+	clearLCDLine(1);                                            // Clear line 2 (1) of the LCD
+
+	//Display the Primary Robot battery voltage
+	displayLCDString(0, 0, "Primary: ");
+	sprintf(mainBattery, "%1.2f%c", nImmediateBatteryLevel/1000.0,'V'); //Build the value to be displayed
+	displayNextLCDString(mainBattery);
+
+	//Display the Backup battery voltage
+	displayLCDString(1, 0, "Backup: ");
+	sprintf(backupBattery, "%1.2f%c", BackupBatteryLevel/1000.0, 'V');    //Build the value to be displayed
+	displayNextLCDString(backupBattery);
+
+	//Short delay for the LCD refresh rate
+	wait1Msec(2000);
+
+	//Declare count variable to keep track of our choice
+	int count = 0;
+
+	//------------- Beginning of User Interface Code ---------------
+	//Clear LCD
+	clearLCDLine(0);
+	clearLCDLine(1);
+	//Loop while center button is not pressed
+	while(nLCDButtons != centerButton)
+	{
+		//Switch case that allows the user to choose from 4 different options
+		switch(count)
+		{
+		case 0:
+			//Display first choice
+			displayLCDCenteredString(0, "Autonomous 1");
+			displayLCDCenteredString(1, "<    Enter   >");
+			waitForPress();
+			//Increment or decrement "count" based on button press
+			if(nLCDButtons == leftButton)
+			{
+				waitForRelease();
+				count = 3;
+			}
+			else if(nLCDButtons == rightButton)
+			{
+				waitForRelease();
+				count++;
+				} else if(nLCDButtons == centerButton) {
+				startTask(usercontrol);
+			}
+			break;
+		case 1:
+			//Display second choice
+			displayLCDCenteredString(0, "Autonomous 2");
+			displayLCDCenteredString(1, "<    Enter   >");
+			waitForPress();
+			//Increment or decrement "count" based on button press
+			if(nLCDButtons == leftButton)
+			{
+				waitForRelease();
+				count--;
+			}
+			else if(nLCDButtons == rightButton)
+			{
+				waitForRelease();
+				count++;
+				} else if(nLCDButtons == centerButton) {
+				autonNumber = 0;
+				startTask(autonomous);
+			}
+			break;
+		case 2:
+			//Display third choice
+			displayLCDCenteredString(0, "Autonomous 3");
+			displayLCDCenteredString(1, "<    Enter   >");
+			waitForPress();
+			//Increment or decrement "count" based on button press
+			if(nLCDButtons == leftButton)
+			{
+				waitForRelease();
+				count--;
+			}
+			else if(nLCDButtons == rightButton)
+			{
+				waitForRelease();
+				count++;
+				} else if(nLCDButtons == centerButton) {
+				autonNumber = 1;
+				startTask(autonomous);
+			}
+			break;
+		case 3:
+			//Display fourth choice
+			displayLCDCenteredString(0, "Autonomous 4");
+			displayLCDCenteredString(1, "<    Enter   >");
+			waitForPress();
+			//Increment or decrement "count" based on button press
+			if(nLCDButtons == leftButton)
+			{
+				waitForRelease();
+				count--;
+			}
+			else if(nLCDButtons == rightButton)
+			{
+				waitForRelease();
+				count = 0;
+				} else if(nLCDButtons == centerButton) {
+				autonNumber = 2;
+				startTask(autonomous);
+			}
+			break;
+		default:
+			count = 0;
+			break;
+		}
+	}
 }
 /*---------------------------Autonmous program starts here------------------------------*/
 void base(int left, int right, int time)//base function for autonomous
@@ -61,20 +197,50 @@ void claw(int speed, int time)//claw function
 
 task autonomous()
 {
-	lift(100, 100);
-	claw(127, 100);
-	lift(100, 1200);
-	claw(127, 200);
-	base(127, 127, 2000);
-	base(0, 0, 500);
-	//base(-127, -127, 700);
-	//lift(100, 1100);
-	//base(127, -127, 500);
-	//base(127, 127, 700);
-	//claw(127, 500);
-	//lift(127, 1000);
-	//base(127, 127, 500);
-	//claw(-127, 500);
+	switch(autonNumber) {
+	case 0://First Autonomous
+		while(true)
+		{
+			clearLCDLine(0);
+			displayLCDCenteredString(0, "Auton 1 Selected");
+			lift(100, 100);
+			claw(127, 100);
+			lift(100, 1200);
+			claw(127, 200);
+			base(127, 127, 2000);
+			base(0, 0, 500);
+			//base(-127, -127, 700);
+			//lift(100, 1100);
+			//base(127, -127, 500);
+			//base(127, 127, 700);
+			//claw(127, 500);
+			//lift(127, 1000);
+			//base(127, 127, 500);
+			//claw(-127, 500);
+		}
+		break;
+	case 1://Second Autonomous
+		while(true)
+		{
+			clearLCDLine(0);
+			displayLCDCenteredString(0, "Auton 2 Selected");
+		}
+		break;
+	case 2://Third Autonomous
+		while(true)
+		{
+			clearLCDLine(0);
+			displayLCDCenteredString(0, "Auton 3 Selected");
+		}
+		break;
+	case 3:
+		while(true)
+		{
+			clearLCDLine(0);
+			displayLCDCenteredString(0, "Auton 4 Selected");
+		}
+	}
+	AutonomousCodePlaceholderForTesting();
 }
 /*-----------------------------Autonomouse ends here----------------------------------*/
 
@@ -85,10 +251,9 @@ int threshold(int number, int threshold = 50)
 }
 task usercontrol()
 {
-  // User control code here, inside the loop
-
-  while (true)
-  {
+	// User control code here, inside the loop
+	while (true)
+	{
 		//lift
 		motor[liftLeft1] = threshold(vexRT[Ch3Xmtr2]);//Ch3: all four motors run at the same time
 		motor[liftRight1] = threshold(vexRT[Ch3Xmtr2]);
@@ -107,7 +272,7 @@ task usercontrol()
 		}
 		else//if nothing is pressed, run the folowing program
 		{
-			motor[collectorLeft] = 0;
+			motor[collectorLeft] = 0;//stop the claw
 			motor[collectorRight] = 0;
 		}
 		//base
@@ -115,6 +280,6 @@ task usercontrol()
 		motor[backRight] = vexRT[Ch3] - vexRT[Ch1];//Ch1: make the base turn left or right
 		motor[frontLeft] = vexRT[Ch3] + vexRT[Ch1];//Ch3/Ch1: one side disable, another side can still turn
 		motor[frontRight] = vexRT[Ch3] - vexRT[Ch1];
-    UserControlCodePlaceholderForTesting();
-  }
+		UserControlCodePlaceholderForTesting();
+	}
 }
